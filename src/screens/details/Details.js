167c988c25ae323baad4b6d1a3aeb366f3dbe403
Chandrika <gehini1302@gmail.com>
2021-07-09 12:54:40 +0530

    Added rating on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -5,12 +5,43 @@ import moviesData from "../../common/movieData";
 import "./Details.css";
 import { Typography } from "@material-ui/core";
 import Home from "../home/Home";
-
+import YouTube from "react-youtube";
+import { ImageList } from "@material-ui/core";
+import { ImageListItemBar } from "@material-ui/core";
+import { ImageListItem } from "@material-ui/core";
+import StarBorderIcon from '@material-ui/icons/StarBorder'
 class Details extends Component {
   constructor() {
     super();
     this.state = {
       movie: {},
+      starIcons: [
+        {
+          id: 1,
+          stateId: "star1",
+          color: "black",
+        },
+        {
+          id: 2,
+          stateId: "star2",
+          color: "black",
+        },
+        {
+          id: 3,
+          stateId: "star3",
+          color: "black",
+        },
+        {
+          id: 4,
+          stateId: "star4",
+          color: "black",
+        },
+        {
+          id: 5,
+          stateId: "star5",
+          color: "black",
+        },
+      ],
     };
   }
   componentWillMount() {
@@ -24,9 +55,28 @@ class Details extends Component {
   backtohomeHandler = () => {
     ReactDOM.render(<Home />, document.getElementById("root"));
   };
-
+  starClickHandler = (id) => {
+    let starIconList = [];
+    for (let star of this.state.starIcons) {
+      let starNode = star;
+      if (star.id <= id) {
+        starNode.color = "yellow";
+      } else {
+        starNode.color = "black";
+      }
+      starIconList.push(starNode);
+    }
+    this.setState({ starIcons: starIconList });
+  };
   render() {
     let movie = this.state.movie;
+    const opts = {
+      height: "450",
+      width: "700",
+      playerVars: {
+        autoplay: 1,
+      },
+    };
     return (
       <div className="details">
         <Header />
@@ -72,8 +122,54 @@ class Details extends Component {
                 <a href={movie.wiki_url}>(Wiki Link)</a> {movie.storyline}{" "}
               </Typography>
             </div>
+            <div className="trailerContainer">
+              <Typography>
+                <span className="bold">Trailer:</span>
+              </Typography>
+              <YouTube
+                videoId={movie.trailer_url.split("?v=")[1]}
+                opts={opts}
+                onReady={this._onReady}
+              />
+            </div>
           </div>
-          <div className="rightDetails"></div>
+          <div className="rightDetails">
+            <Typography>
+              <span className="bold">Rate this movie:</span>
+            </Typography>
+            {this.state.starIcons.map((star) => (
+              <StarBorderIcon
+                className={star.color}
+                key={"star" + star.id}
+                onClick={() => this.starClickHandler(star.id)}
+              />
+            ))}
+            <div className="bold marginBottom16 marginTop16">
+              <Typography>
+                <span className="bold">Artists:</span>
+              </Typography>
+            </div>
+            <div className="paddingRight">
+              <ImageList cellHeight={160} cols={2}>
+                {movie.artists != null &&
+                  movie.artists.map((artist) => (
+                    <ImageListItem
+                      className="gridTile"
+                      onClick={() => this.artistClickHandler(artist.wiki_url)}
+                      key={artist.id}
+                    >
+                      <img
+                        src={artist.profile_url}
+                        alt={artist.first_name + " " + artist.last_name}
+                      />
+                      <ImageListItemBar
+                        title={artist.first_name + " " + artist.last_name}
+                      />
+                    </ImageListItem>
+                  ))}
+              </ImageList>
+            </div>
+          </div>
         </div>
       </div>
     );
